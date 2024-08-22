09cbcdf823d6893ba2c3d8a8ad171dbe25502cb0
Janice Chen <janicekchen@gmail.com>
2024-08-21 20:46:27 -0700

    do things

---
+++ src/lib/card.svelte
@@ -0,0 +1,28 @@
+<script>
+    let title = "bop"
+    let subtitle = "beep boop bap"
+</script>
+
+<div class=card-container>
+    <div class=thumbnail></div>
+    <h3>{title}</h3>
+    <h4>{subtitle}</h4>
+
+</div>
+
+
+<style>
+    h3 {
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+
+    .card-container {
+        border: 1px solid orange;
+        max-width: 300px;
+    }
+
+    .thumbnail {
+        height: 150px;
+        background-color: 'lavender';
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Card from '$lib/card.svelte'
+</script>
+<div class="container">
+    <h1>janice kai chen</h1>
+    <p>my bio goes here</p>
+    <h2>selected work</h2>
+    <div class="selected-works">
+        <Card/>
+        <Card/>
+    </div>
+</div>
+
+<style>
+    h1 {
+        font-family: 'Arial'
+    }
+
+    .container {
+        width: 640px;
+        margin: auto;
+        background-color: aliceblue;
+    }
+
+    .selected-works {
+        display: 'flex';
+        flex-wrap: 'wrap';
+
+    }
+</style>
+
