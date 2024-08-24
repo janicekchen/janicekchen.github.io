79b7990e1c4bcef6c42f3c5113e2ad2c2f166b9b
janicekchen <janicekchen@gmail.com>
2024-08-23 22:53:23 -0700

    css grid for mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,9 @@
 </script>
 <div class="container">
     <p class="bio">
-        I'm a journalist, data viz designer, and cartographer based in Seattle. I also make things, sometimes. 
+        I'm a journalist, data viz designer, and cartographer based in Seattle. I am often on sidequests (rabidly learning things to realize ridiculous projects). 
     </p>
-    <h2>selected work</h2>
+    <h2>selected work [what is visual hierarchy?!]</h2>
     <div class="selected-works">
         <Card/>
         <Card/>
@@ -19,15 +19,21 @@
         font-size: 1.25em;
         font-family: var(--font-secondary);
         font-weight: 300;
+        max-width: 640px;
+        margin: auto;
     }
     h2 {
-        font-size: 1em;
+        font-size: 0.5em;
         font-weight: 700;
         text-transform: uppercase;
+        margin: 50px auto;
+        max-width: 640px;
+        text-align: center;
+        text-decoration: underline wavy red;
+
     }
 
     .container {
-        width: 640px;
         margin: auto;
         /*background-color: aliceblue;*/
         font-family: var(--font-primary)
@@ -35,9 +41,15 @@
 
     .selected-works {
         width: 100%;
+        max-width: 960px;
+        margin: auto;
         display: grid;
-        grid-template-columns: minmax(auto, 1fr) minmax(auto, 1fr);
+        grid-template-columns: minmax(auto, 1fr) minmax(auto, 1fr) minmax(auto, 1fr);
         gap: 10px 20px;
+
+        @media (max-width: 550px) {
+            grid-template-columns: minmax(auto, 1fr);
+        }
         
 
     }
