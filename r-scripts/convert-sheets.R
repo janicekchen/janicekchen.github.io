cde4f3114b8858e4a7cb48d082e593afa0c892e5
Janice Chen <janicekchen@gmail.com>
2024-12-29 15:32:03 -0800

    start photo processing

--- r-scripts/convert-sheets.R
+++ r-scripts/convert-sheets.R
@@ -3,6 +3,7 @@ suppressMessages(library(dplyr))
 
 # convert portfolio data to json
 data <- read.csv("src/lib/data/portfolio-data.csv") 
+row.names(data) <- NULL
 data_list <- split(data, seq(nrow(data)))
 
 data_json <- toJSON(data_list, pretty=TRUE) 
--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,5 +1,26 @@
+<script>
+    import cards from '../../lib/data/portfolio-data.json'
+    import Card from '../../lib/card.svelte';
+
+    const imageModules = import.meta.glob("../../lib/img/work/*.jpg");
+    console.log(imageModules)
+    for (const modulePath in imageModules) {
+    imageModules[modulePath]().then(({ default: imageUrl }) => {
+      console.log(modulePath, imageUrl);
+    });
+  }
+
+
+    $: console.log(cards)
+</script>
 <div class="cards-container">
-    
+    {#each cards as card}
+        <Card 
+            hed={card.hed} 
+            url={card.link}
+            --imgUrl={`url(${'$lib/img/work/' + card.thumbnail +'.jpg'})`}
+        />
+    {/each}
 
 
 </div>
