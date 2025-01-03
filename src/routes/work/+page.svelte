aaaed0d210015a02e2028c224830eab07f4c35fd
Janice Chen <janicekchen@gmail.com>
2025-01-03 12:11:34 -0800

    rm glob

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -2,16 +2,14 @@
     import cards from '../../lib/data/portfolio-data.json'
     import Card from '../../lib/card.svelte';
 
-    const imageModules = import.meta.glob("../../lib/img/work/*.jpg");
-    console.log(imageModules)
-    for (const modulePath in imageModules) {
-    imageModules[modulePath]().then(({ default: imageUrl }) => {
-      console.log(modulePath, imageUrl);
-    });
-  }
+//     const imageModules = import.meta.glob("../../lib/img/work/*.jpg");
+//     console.log(imageModules)
+//     for (const modulePath in imageModules) {
+//     imageModules[modulePath]().then(({ default: imageUrl }) => {
+//       console.log(modulePath, imageUrl);
+//     });
+//   }
 
-
-    $: console.log(cards)
 </script>
 <div class="cards-container">
     {#each cards as card}
