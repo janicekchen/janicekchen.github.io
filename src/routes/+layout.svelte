863575abedfe8475da4d224162ddca6c131a2523
Janice Chen <janicekchen@gmail.com>
2025-01-07 11:44:53 -0800

    feat: under construction page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,90 +12,9 @@
     $: console.log(path)
     
 </script>
-<header>
-    <h1>
-        <a href="{base}/">janice kai chen</a>
-    </h1>
-    <div class="nav-wrapper">
-        <nav>
-            <ul>
-                <li><a class:is-active={path === `/work`} href="{base}/work">professional work</a></li>
-                <li><a class:is-active={path === `/sidequests`} href="{base}/sidequests">sidequests</a></li>
-            </ul>
-        </nav>
-    </div>
-</header>
-
 
 <slot></slot>
 
 <style>
-    header {
-        margin: auto;
-        padding: 30px 0;
-        color: var(--text-color-primary);
-        max-width: 640px;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        letter-spacing: 1px;
-        top: 0; 
-        font-family: var(--font-primary);
-        font-weight: bold;
-
-        @media (max-width: 550px) {
-            flex-direction: column;
-        }
-    }
-
-    a, a:visited, a:active {
-        color: var(--text-color-primary);
-        text-decoration: none;
-        padding: 20px 0px;
-        /* text-decoration-line: underline;
-        text-decoration-thickness: 1px;
-        text-decoration-color: greenyellow;
-        -webkit-text-decoration-line: underline;
-        -webkit-text-decoration-thickness: 3px;
-        -webkit-text-decoration-color: greenyellow; */
-    }
-
-    .is-active {
-        text-decoration: underline;
-        /* text-decoration-style:wavy; */
-    }
-
-    a:hover {
-        color: deeppink;
-        text-decoration-line: underline;
-        text-decoration-style: wavy;
-        text-decoration-thickness: 1px;
-        text-decoration-color: deeppink;
-        -webkit-text-decoration-line: underline;
-        -webkit-text-decoration-style: wavy;
-        -webkit-text-decoration-thickness: 3px;
-        -webkit-text-decoration-color: deeppink;
-    }
-
-    h1 {
-        font-size: 1.25em;
-        font-weight: bold;
-    }
-
-    ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
-        right: 0;
-        display: flex;
-        gap: 30px;
-        font-size: 0.75em;
-        text-decoration: none;
-        color: #000;
-    }
-
-    li {
-        
-    }
 
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,89 +1,44 @@
 <script>
     import Card from '$lib/card.svelte'
-    // import img1 from '$lib/img/selected_1.png'
-    // import img2 from '$lib/img/selected_2.png'
+    // import img1 from '/img/selected_1.png'
+    // import img2 from '/img/selected_2.png'
     // import img3 from '$lib/img/selected_3.png'
     import { base } from '$app/paths';
 </script>
 <div class="container">
-    <p class="bio">
-        I'm a journalist, data viz designer, and cartographer based in Seattle. Full time at the Washington Post; part time on <a href="{base}/sidequests">sidequests</a>. 
-    </p>
-    <p></p>
-<!-- <p class="bio">hello [at] janicekchen.com</p> -->
-    <h2>selected work</h2>
-    <div class="selected-works">
-        <Card 
-            hed={"State affirmative action bans helped White, Asian students, hurt others"} 
-            subhed="The Washington Post" 
-            url="https://www.washingtonpost.com/education/2023/06/29/affirmative-action-banned-what-happens/?itid=ap_janicekaichen"
-            --imgUrl={`url(/img/selected_1.png)`}/>
-        <Card 
-        hed={"Pigeons are still (sometimes) faster than your internet"} 
-        subhed="The Washington Post" 
-        url="https://www.washingtonpost.com/technology/2023/11/10/pigeons-are-faster-than-your-internet/?itid=ap_janicekaichen"
-        --imgUrl={`url(/img/selected_2.png)`}/>
-        <Card 
-            hed={"Milk Country"} 
-            subhed={"a story about cows, history and familiar landscapes"} 
-            url={"https://vermont.vercel.app"}
-            --imgUrl={`url(/img/selected_3.png)`}/>
-    </div>
+    <img src={`/img/under-construction.png`} alt="beep"/>
+    <p>portfolio under construction.<br>please find my latest work <a href="https://www.washingtonpost.com/people/janice-kai-chen/">here</a>.</p>
 </div>
 
 <style>
-
-    mark {
-        color: deeppink;
+    a {
+        text-decoration: none;
+        color: deeppink
+    }
+    p {
+        font-size: 14px
     }
 
-    .bio {
-        font-weight: 300;
-        font-size: 14px;
-        line-height: 25px;
-        font-family: var(--font-secondary);
-        color: var(--text-color-primary);
-        font-weight: 300;
-        max-width: 640px;
-        margin: auto;
-        /* text-align: center; */
+    img {
+        width: 200px;
     }
-
-    h2 {
-        font-size: 1em;
-        font-weight: 400;
-        color: var(--text-color-primary);
-        letter-spacing: 1px;
-        /* text-transform: uppercase; */
-        margin: 50px auto 10px auto;
-        max-width: 640px;
-        /* text-decoration-style: wavy;
-        text-decoration-line: underline;
-        text-decoration-thickness: 0.5px; */
-        /* text-align: center; */
-
-
-    }
-
     .container {
         margin: auto;
         /*background-color: aliceblue;*/
-        font-family: var(--font-primary)
+        font-family: var(--font-primary);
+        width: 100vw;
+        height: 100vh;
+        padding: 20px;
+        overflow: hidden;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        text-align: center;
     }
 
-    .selected-works {
-        width: 100%;
-        max-width: 640px;
-        margin: auto;
-        display: grid;
-        grid-template-columns: repeat(2, 1fr);
-        gap: 10px 20px;
+ 
 
-        @media (max-width: 550px) {
-            grid-template-columns: minmax(auto, 1fr);
-        }
-        
-
-    }
 </style>
 
