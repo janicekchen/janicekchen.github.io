cabe77d1c8333d3ec74aecfceea0b106289ccce2
Janice Chen <janicekchen@gmail.com>
2025-01-20 18:26:18 +0800

    sidequests shenanigans

--- src/lib/sidequest.svelte
+++ src/lib/sidequest.svelte
@@ -1,67 +1,67 @@
 <script>
-    import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
-    import '@splidejs/svelte-splide/css'
+	import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+	import '@splidejs/svelte-splide/css';
 	export let hed;
 	export let url;
-    export let img;
+	export let img;
 </script>
 
 <Splide aria-label="My Favorite Images">
-        <SplideSlide>
-            <img class="slide" src={'img/sidequests/bb_1.jpg'} alt="sdf1"/>
-        </SplideSlide>
-        <SplideSlide>
-            <img class="slide" src={'img/sidequests/bb_2.jpg'} alt="sdf1"/>
-        </SplideSlide>
-  </Splide>
-<div class="sidequest-container">
-	<div class="desc"></div>
-
+	<SplideSlide>
+		<img class="slide" src={'img/sidequests/bb_1.jpg'} alt="sdf1" />
+	</SplideSlide>
+	<SplideSlide>
+		<img class="slide" src={'img/sidequests/bb_2.jpg'} alt="sdf1" />
+	</SplideSlide>
+</Splide>
+<div class="desc">
+	<h3 class="sidequest-title">
+		<mark> Bivariate Bivalves: Electronic Map </mark>
+        <!-- <mark>{hed}</mark> -->
+	</h3>
+	<p class="sidequest-desc">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut mollis, nulla ut elementum rhoncus,
+		urna diam iaculis nisl, sed lacinia libero lectus at dolor. Maecenas rhoncus lobortis diam, in
+		porttitor quam aliquet eu. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+		at justo ac nunc sagittis maximus. In hac habitasse platea dictumst. 
+	</p>
 </div>
 
 <style>
-	.sidequest-container {
+	.slide {
+		max-width: 640px;
 		width: 100%;
-		height: 500px;
-		background-color: aliceblue;
-		padding: 10px;
-		box-sizing: border-box;
+		aspect-ratio: 1;
+		object-fit: cover;
 	}
 
-    .slide {
-        max-width: 640px;
-        width: 100%;
-        aspect-ratio: 1;
-        object-fit: cover;
-    }
-
-	.card {
-		width: 100%;
-        aspect-ratio: 1;
-		background-color: lightseagreen;
-	}
-
-    mark {
+	mark {
 		background-color: #fafdf2;
-        color: var(--text-color-primary);
-        /* padding: 5px 5px; */
-        /* display:inline-block; */
+		color: var(--text-color-primary);
+		/* padding: 5px 5px; */
+		/* display:inline-block; */
 	}
 
-    h3 {
+	h3 {
 		font-family: var(--font-primary);
 		color: var(--text-color-primary);
 		/* margin: 10px 0px 0px 0px; */
-		margin: 5px;
 		font-size: 0.75em;
 		letter-spacing: 0.25px;
 		line-height: 1.25em;
 		/* min-height: 30px; */
 	}
 
+    p {
+        font-family: var(--font-primary);
+        font-size: 0.75em;
+    }
+
 	.desc {
-		background-color: tomato;
+		/* background-color: /tomato; */
 		width: 100%;
-		height: 30px;
+		height: auto;
+        padding: 0px 10px;
+        box-sizing: border-box;
 	}
 </style>
